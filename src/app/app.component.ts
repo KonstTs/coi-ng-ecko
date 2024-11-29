a9b66693b1add0df73d8c77dc73f65ab1055f3ed
KonstTs <tsolakidis.konst@gmail.com>
2024-11-29 03:09:40 +0200

    conflicts resolution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PfDashboardViewModelService } from './views/dashboard/dashboard-viewmodel.service';
-import { PfTableViewModelService } from './shared/structure/table/table-viewmodel.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { PfCoingeckoService } from './api/services/coins-services.service';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
