b95c9f17a4d036e2fd940376495654fd5c17fe84
KonstTs <tsolakidis.konst@gmail.com>
2024-12-13 09:50:57 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   loggerLevel: 4,
-  coingeckoApiKey: 'CG-x8sLogniDaJxuDFZMWf9z6it'
+  coingeckoApiKey: 'CG-x8sLogniDaJxuDFZMWf9z6it',
+  coinGeckoBaseURL: 'https://api.coingecko.com'
 };
